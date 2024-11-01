597cf7ddaaee273d5141b0cccf1b25622ae711e7
n0o01lh <92607073+n0o01lh@users.noreply.github.com>
2024-11-01 00:42:11 -0300

    adds resources sales component (#13)

---
+++ src/components/Sales/constants.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  RESOURCES: "#8884D8",
+  COURSES: "#82CA9D",
+};
+
+export const LEGEND_STYLE = {
+  top: "50%",
+  left: 0,
+  transform: "translate(0, -50%)",
+  lineHeight: "24px",
+};
