8e7e4a2be986cf8a6ae5612ef864ad45e10edb49
Nicolas Onate <nionate.on@gmail.com>
2024-11-01 16:05:43 -0300

    adds courses sales component and graph data

---
+++ src/components/Sales/Sales.interfaces.tsx
@@ -0,0 +1,13 @@
+export interface ResourceSales {
+  amount: number;
+  resourceId: number;
+  teacherId: number;
+  title: string;
+}
+
+export interface CourseSales {
+  amount: number;
+  courseId: number;
+  teacherId: number;
+  title: string;
+}
